f079b28386346979a641a31e4cbcc142ac36ba36
CyberWarrior5466 <86358454+CyberWarrior5466@users.noreply.github.com>
2024-06-01 23:42:39 +0100

    neovim config

--- config.fish
+++ config.fish
@@ -1,10 +1,7 @@
+alias c++="g++-14"
+alias cc="gcc-14"
 alias code="flatpak run com.vscodium.codium &> /dev/null"
 alias find="fdfind"
 alias grep="rg"
 alias ls="eza"
 alias tree="eza --tree"
-
-# if set -q KITTY_PID; and not set -q NVIM
-#     nvim --cmd ":terminal"
-#     exit
-# end
---
+++ nvim/lua/config.lua
@@ -0,0 +1,135 @@
+vim.cmd.colorscheme = "default"
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\" -- same as mapleader
+vim.g.have_nerd_font = false
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = "a"
+vim.o.breakindent = true
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.autoindent = true
+vim.o.smarttab = true
+vim.o.expandtab = true
+
+vim.o.signcolumn = "yes"
+
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.o.inccommand = 'split'
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+
+vim.o.hlsearch = true
+
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      { "folke/neodev.nvim", opts = {} },
+    },
+    config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+      local servers = { lua_ls = { settings = { Lua = { completion = { callSnippet = "Replace" } } } } }
+
+      require("mason").setup()
+      local ensure_installed = vim.tbl_keys(servers or {})
+
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+      require("mason-lspconfig").setup({
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            require("lspconfig")[server_name].setup(server)
+          end,
+        },
+      })
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      luasnip.config.setup({})
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = "menu,menuone,noinsert" },
+
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete({}),
+
+          ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { "i", "s" }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+
+  "Pocco81/auto-save.nvim",
+}, {})
+
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+  command = "startinsert"
+})
+
+vim.cmd(":terminal")
+
